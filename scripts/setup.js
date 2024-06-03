985037e80d2dffebcd05a99e93f292fb6d855056
Milko Škofič <milko@skofic.io>
2024-06-03 20:52:17 +0200

    Trying to make setupwork: changed log messages from exporting log message list to console.debug().

--- scripts/setup.js
+++ scripts/setup.js
@@ -12,39 +12,34 @@ const {context} = require('@arangodb/locals')
 const database = require('../globals/database')
 
 ///
-// Lod.
-///
-let log = ""
-
-///
 // Handle document collections.
 ///
 for (const [key, collection] of Object.entries(database.globals.collections)) {
 	if (!db._collection(collection.name)) {
-		log +=`Creating document collection ${collection.name}.\n`
+		console.debug(`Creating document collection ${collection.name}.\n`)
 		const coll = db._createDocumentCollection(collection.name)
 		for(const index of collection.index) {
 			coll.ensureIndex(index)
 		}
 	} else if (context.isProduction) {
-		log += `Collection ${collection.name} already exists. Leaving it untouched.\n`
+		console.debug(`Collection ${collection.name} already exists. Leaving it untouched.\n`)
 	}
 }
 
-// ///
-// // Create edge collections.
-// ///
-// for (const [key, collection] of Object.entries(database.globals.edges)) {
-// 	if (!db._collection(collection.name)) {
-// 		log +=`Creating edge collection ${collection.name}.\n`
-// 		const coll = db._createEdgeCollection(collection.name)
-// 		for(const index of collection.index) {
-// 			coll.ensureIndex(index)
-// 		}
-// 	} else if (context.isProduction) {
-// 		log += `Collection ${collection.name} already exists. Leaving it untouched.\n`
-// 	}
-// }
+///
+// Create edge collections.
+///
+for (const [key, collection] of Object.entries(database.globals.edges)) {
+	if (!db._collection(collection.name)) {
+		console.debug(`Creating edge collection ${collection.name}.\n`)
+		const coll = db._createEdgeCollection(collection.name)
+		for(const index of collection.index) {
+			coll.ensureIndex(index)
+		}
+	} else if (context.isProduction) {
+		console.debug(`Collection ${collection.name} already exists. Leaving it untouched.\n`)
+	}
+}
 
 ///
 // Create analyzers.
@@ -68,5 +63,3 @@ for (const [key, value] of Object.entries(database.globals.views)) {
 		db._createView(value.name, value.type, value.properties)
 	}
 }
-
-module.exports = log
